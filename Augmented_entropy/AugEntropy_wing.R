26ad81db905197f5d59f2f3445b2b9463c914eec
Yao Xiao <95415957+zuelxy@users.noreply.github.com>
2023-04-08 23:41:36 +0800

    Add files via upload

---
+++ Augmented_entropy/AugEntropy_wing.R
@@ -0,0 +1,52 @@
+setwd("~/Desktop/R_script/Augmented_UD/Wingweight")
+library(DiceKriging)
+wingweight <- function(xx)
+{
+  Sw      <- 150+(200-150)*xx[1]
+  Wfw     <- 220+(300-220)*xx[2]
+  A       <- 6+(10-6)*xx[3]
+  LamCaps <- ((-10)+(10-(-10))*xx[4]) * (pi/180)
+  q       <- 16+(45-16)*xx[5]
+  lam     <- 0.5+(1-0.5)*xx[6]
+  tc      <- 0.08+(0.18-0.08)*xx[7]
+  Nz      <- 2.5+(6-2.5)*xx[8]
+  Wdg     <- 1700+(2500-1700)*xx[9]
+  Wp      <- 0.025+(0.08-0.025)*xx[10]
+  
+  fact1 <- 0.036 * Sw^0.758 * Wfw^0.0035
+  fact2 <- (A / ((cos(LamCaps))^2))^0.6
+  fact3 <- q^0.006 * lam^0.04
+  fact4 <- (100*tc / cos(LamCaps))^(-0.3)
+  fact5 <- (Nz*Wdg)^0.49
+  
+  term1 <- Sw * Wp
+  
+  y <- fact1*fact2*fact3*fact4*fact5 + term1
+  return(y)
+}
+wing_score <- function(design, output, newpoint, newout, seed){
+  set.seed(seed)
+  m <- km(~., design=design, response=output)
+  p <- predict(m, newpoint,"UK")
+  y1 <- sqrt(sum((p$mean-newout)^2)/nrow(newpoint))
+  y2 <- median(abs(p$mean-newout))
+  return(list(rmspe=y1, mar=y2))
+}
+
+### test point 
+wingpoint <- as.matrix(read.table("testpoint10.txt",header = T))
+wingout <- apply(wingpoint,1,wingweight)
+
+### initial design 
+X0 <- as.matrix(read.table("UD35_10.txt",header = T))
+
+### max entropy
+max_entropy <- as.matrix(read.table("AugEntropy_65.txt", header = T))
+Aug_entrop <- rbind(X0,max_entropy)
+rownames(Aug_entrop) <- 1:100
+entropy_y <- apply(Aug_entrop, 1, wingweight)
+
+result_entropy <- wing_score(Aug_entrop, entropy_y, wingpoint, 
+                             wingout, seed = 13)
+# [1] 1.828471
+# [1] 0.7969446
